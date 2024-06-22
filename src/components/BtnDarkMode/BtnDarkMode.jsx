5278f6808685a6baee8ddf24a60afd21c782c355
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2024-06-22 12:20:26 +0300

    add dark mode

--- src/components/BtnDarkMode/BtnDarkMode.jsx
+++ src/components/BtnDarkMode/BtnDarkMode.jsx
@@ -1,20 +1,33 @@
-import { useEffect, useState } from 'react'
-import './BtnDarkMode.scss'
+import { useEffect, useRef, useState } from 'react'
+import { detectDarkMode } from '../../utils/detectDarkMode'
 
+import './BtnDarkMode.scss'
 import moon from '/icons/moon.svg'
 import sun from '/icons/sun.svg'
 
 const BtnDarkMode = () => {
-	const [isDarkMode, setIsDarkMode] = useState(false)
+	const [isDarkMode, setIsDarkMode] = useState(detectDarkMode)
+	const btnRef = useRef(null)
 
-	useEffect(() => {}, [isDarkMode])
+	useEffect(() => {
+		if (isDarkMode === 'dark') {
+			btnRef.current.classList.add('dark-mode-btn--active')
+			document.body.classList.add('dark')
+		} else {
+			btnRef.current.classList.remove('dark-mode-btn--active')
+
+			document.body.classList.remove('dark')
+		}
+	}, [isDarkMode])
 
 	function toggleDarkMode() {
-		setIsDarkMode((current) => setIsDarkMode(!current))
+		setIsDarkMode((currentValue) => {
+			return currentValue === 'light' ? 'dark' : 'light'
+		})
 	}
 
 	return (
-		<button className='dark-mode-btn' onClick={toggleDarkMode}>
+		<button ref={btnRef} className='dark-mode-btn' onClick={toggleDarkMode}>
 			<img src={sun} alt='Light mode' className='dark-mode-btn__icon' />
 			<img src={moon} alt='Dark mode' className='dark-mode-btn__icon' />
 		</button>
