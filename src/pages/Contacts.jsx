0367aaf4a13e3f61c97b25ccf1bbc8a9ea1f955e
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2024-06-22 12:27:48 +0300

    change Contacts page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -7,18 +7,18 @@ const Contacts = () => {
 				<ul className='content-list'>
 					<li className='content-list__item'>
 						<h2 className='title-2'>Location</h2>
-						<p>Moscow, Russia</p>
+						<p>Russia</p>
 					</li>
 					<li className='content-list__item'>
 						<h2 className='title-2'>Telegram / WhatsApp</h2>
 						<p>
-							<a href='tel:+79051234567'>+7 (905) 123-45-67</a>
+							<a href='tel:+79870689005'>+7 (987) 068-90-05</a>
 						</p>
 					</li>
 					<li className='content-list__item'>
 						<h2 className='title-2'>Email</h2>
 						<p>
-							<a href='mailto:webdev@protonmail.com'>webdev@protonmail.com</a>
+							<a href='mailto:tatsveet@gmail.com'>tatsveet@gmail.com</a>
 						</p>
 					</li>
 				</ul>
